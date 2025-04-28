3dd2baec9f3f16f1d6b5006a693f093e8fe4a0f9
ClaraY05 <clara.w.yee@gmail.com>
2025-04-28 08:57:04 -0700

    set up outline for project code

---
+++ project_car/project_car.ino
@@ -0,0 +1,33 @@
+// project_car.
+// 
+// * 
+// *    
+// *    
+// *    
+// *  Names: Clara Yee + Vaish Sam
+// *  UID (Clara): 406392066
+// *  UID (Vaish):
+
+// DEFINE INITIAL VALUES
+// define constants
+// define variables + arrays
+// define pins
+
+// SETUP (program run once for initialization)
+void setup(){
+    // set pin modes 
+    // initialize pins
+    // initialize serial communication
+    // set base speed
+}
+
+// LOOP (program run continiously as car is on)
+void loop(){
+    // read sensor Values
+    // check for crosspiece
+    // subtract 8 minimums
+    // ratio 8 values to 1000
+    // use 8 weights to compute error
+    // compute steering change command
+    // add change to one wheel, subtract from other
+}
