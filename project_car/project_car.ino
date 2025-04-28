891fe6d59c7020db13bba2c999750e8cd9e812ce
ClaraY05 <clara.w.yee@gmail.com>
2025-04-28 09:20:47 -0700

    setup compute error helper function

--- project_car/project_car.ino
+++ project_car/project_car.ino
@@ -10,7 +10,11 @@
 
 // DEFINE INITIAL VALUES
 // define constants
+
 // define variables + arrays
+const int sensor_weight[8] = [-15,-14,-12,-8,8,12,14,15];
+int sensor_measured[8] = [0];
+
 // define pins
 
 // SETUP (program run once for initialization)
@@ -28,6 +32,26 @@ void loop(){
     // subtract 8 minimums
     // ratio 8 values to 1000
     // use 8 weights to compute error
+    int error = compute_error();
     // compute steering change command
     // add change to one wheel, subtract from other
+}
+
+// HELPER FUNCTIONS
+
+// computes the error term by fusing each value according to values in sensor_weight
+int compute_error (int sens[8]){
+    return       (sens[0]*sensor_weight[0] 
+                + sens[1]*sensor_weight[1]
+                + sens[2]*sensor_weight[2]
+                + sens[3]*sensor_weight[3]
+                + sens[4]*sensor_weight[4]
+                + sens[5]*sensor_weight[5]
+                + sens[6]*sensor_weight[6]
+                + sens[7]*sensor_weight[7])/8;
+}
+
+// computes steering change according to error determined by weights on track
+void adjust_steering(int error){
+
 }
