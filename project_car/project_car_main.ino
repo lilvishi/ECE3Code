3b6d18a2674e65643e888c426dd5425f50001274
ClaraY05 <clara.w.yee@gmail.com>
2025-05-01 12:56:09 -0700

    added changes to compute_error function

--- project_car/project_car_main.ino
+++ project_car/project_car_main.ino
@@ -12,17 +12,22 @@
 
 // DEFINE INITIAL VALUES
 // define constants
-const int baseSpeed = 25;
+
+// min and max from calibration
+const int sensorMins[8] = {436,527,505,482,550,459,574,527};
+const int sensorMax[8] = {1895,1729,1490,877,1635,1606,1926,1973};
+const int calibrationWeight[8] = {-8,-4,-2,-1,1,2,4,8};
 
 // define variables + arrays
-const int base_speed[8] = [0];
+int sensorCalc[8] ={0};
+int calcError = 0;
+uint16_t sensor_measured[8] = {0};
 
-uint16_t sensor_measured[8] = [0];
-
+int baseSpeed = 25;
 
 // define pins
 const int left_nslp_pin =31;     // nslp ==> awake & ready for PWM
-const int right_nslp_pin=11;    // nslp ==> awake & ready for PWM
+const int right_nslp_pin=11;     // nslp ==> awake & ready for PWM
 const int left_dir_pin  =29;
 const int right_dir_pin=30;
 const int left_pwm_pin=40;
@@ -60,28 +65,46 @@ void loop(){
     ECE3_read_IR(sensor_measured);
 
     // check for crosspiece
-    // subtract 8 minimums
-    // ratio 8 values to 1000
-    // use 8 weights to compute error
-    int error = compute_error();
+
+    // compute error
+    compute_error(sensor_measured);
     // compute steering change command
     // add change to one wheel, subtract from other
 }
 
 // HELPER FUNCTIONS
 
-// computes the error term by fusing each value according to values in sensor_weight
-int compute_error (int sens[8]){
-    // find min
-    return       (sens[0]*sensor_weight[0] 
-                + sens[1]*sensor_weight[1]
-                + sens[2]*sensor_weight[2]
-                + sens[3]*sensor_weight[3]
-                + sens[4]*sensor_weight[4]
-                + sens[5]*sensor_weight[5]
-                + sens[6]*sensor_weight[6]
-                + sens[7]*sensor_weight[7])/8;
+// computes the error term by fusing each value according to values in calibrationWeight
+void compute_error (int sens[8]){
+    // read raw sensor values
+    ECE3_read_IR(sensorValues);
+
+    // subtract mins
+    for(unsigned char i = 0; i < 8; i++){
+        sensorCalc[i] = sensorValues[i] - sensorMins[i];
+    }
+    // normalize to 1000
+    for(unsigned char j = 0; j < 8; j++){
+        sensorCalc[j] = sensorCalc[j]*1000/sensorMax[j];
+    }
+    // calculate error
+    for(unsigned char k = 0; k < 8; k++){
+        calcError += sensorCalc[k] * calibrationWeight[k];
+    }
+    calcError /= 4;
+    return;
 }
 
 // computes steering change according to error determined by weights on track
-void adjust_steeri
+void adjust_steer(){}
+
+// turns right
+void turn_right(){
+    digitalWrite(left_dir_pin,HIGH); 
+    digitalWrite(right_dir_pin,LOW); 
+}
+// turns left
+void turn_left(){
+    digitalWrite(left_dir_pin,LOW); 
+    digitalWrite(right_dir_pin,HIGH); 
+}
