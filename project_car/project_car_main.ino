81adcc6121b683e85814cf0b7d43b4496714f79d
ClaraY05 <clara.w.yee@gmail.com>
2025-05-15 09:28:59 -0700

    adjusted project code

--- project_car/project_car_main.ino
+++ project_car/project_car_main.ino
@@ -13,15 +13,15 @@
 
 // DEFINE INITIAL VALUES
 // define constants
-const float Kp = 0.0085; // determine experimentally
-const float Kd = 0.1;
+const float Kp = 0.03; // determine experimentally
+const float Kd = 0.005;
 const int errorMax = 2777;
 
 // min and max from calibration
 const int sensorMins[8] = {436,527,505,482,550,459,574,527};
 const int sensorMax[8] = {1895,1729,1490,877,1635,1606,1926,1973};
 const int calibrationWeight[8] = {-8,-4,-2,-1,1,2,4,8};
-const int start_speed = 50;
+const int start_speed = 25;
 
 // define variables + arrays
 int sensorCalc[8] ={0};
@@ -30,10 +30,14 @@ float calcError = 0;
 float prevError = 0;
 float dt = 0;
 
+// 14.6 cm in diameter
+int readSum = 0;
+bool isCrosspiece = false;
+
 float last_time = 0;
 float now_time = 0;
-int right_baseSpeed = 50;
-int left_baseSpeed = 50;
+int right_baseSpeed = 25;
+int left_baseSpeed = 25;
 
 // define pins
 const int left_nslp_pin =31;     // nslp ==> awake & ready for PWM
@@ -74,27 +78,29 @@ void loop(){
     // read sensor Values
     ECE3_read_IR(sensor_measured);
 
-    // check for crosspiece
-
     // compute error
-    compute_error(sensor_measured);
-
+    compute_error(sensor_measured); // changes calcError and isCrosspiece
+    // check for crosspiece
+    if(isCrosspiece){
+        turn_right();
+        isCrosspiece = false;
+    }
     // compute steering change command
-    //adjust_steer(calcError);            // adjust the steering
     now_time = millis();
     dt = now_time - last_time;
     last_time = now_time;
 
     int PDval = getPD();
 
+    // add change to one wheel, subtract from other
+
     left_baseSpeed = start_speed - PDval;
     right_baseSpeed = start_speed + PDval;
+    adjust_steer();            // adjust the steering
 
     prevError = calcError;
     analogWrite(left_pwm_pin,left_baseSpeed);
     analogWrite(right_pwm_pin, right_baseSpeed);
-
-    // add change to one wheel, subtract from other
 }
 
 // HELPER FUNCTIONS
@@ -114,9 +120,55 @@ void compute_error (uint16_t sensorValues[8]){
         calcError += sensorCalc[k] * calibrationWeight[k];
     }
     calcError /= 4;
+
+    readSum = 0;
+    isCrosspiece = false;
+    for(int i = 0; i < 8 ; i++){
+        readSum += sensor_measured[i];
+    }
+
+    readSum /= 8;
+    if (readSum > 900)
+        isCrosspiece = true;
+    return;
+}
+// computes steering change according to error determined by weights on track
+void adjust_steer(){
+    if (calcError > 200) { // left wheel slower
+        turn_left();
+        }
+    else if(calcError < -200) { // right wheel slower
+        turn_right();
+        }
+    else{
+        forward();;
+        }
     return;
 }
 
+// turns right
+void turn_right(){
+    digitalWrite(left_dir_pin,LOW); 
+    digitalWrite(right_dir_pin,HIGH); 
+    if(right_baseSpeed < 0){
+        right_baseSpeed *= -1;
+        //right_baseSpeed /= 2;
+    }
+}
+// turns left
+void turn_left(){
+    digitalWrite(left_dir_pin,HIGH); 
+    digitalWrite(right_dir_pin,LOW); 
+    if(left_baseSpeed < 0){
+        left_baseSpeed *= -1;
+        //left_baseSpeed /= 2;
+    }
+}
+// goes forward
+void forward(){
+    digitalWrite(left_dir_pin,LOW); 
+    digitalWrite(right_dir_pin,LOW); 
+}
 
 // calc initial PD
 int getPD(){
